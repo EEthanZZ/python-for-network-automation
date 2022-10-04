b0e108e2893d6794c1d3c6cc9e10b6fa86f3ced1
Ethan <zycc2727@gmail.com>
2022-10-04 22:52:18 +1100

    asista devices os & versions

--- 02_devices.py
+++ 02_devices.py
@@ -4,7 +4,7 @@ import string
 
 devices = []
 
-for i in range(0, 5):
+for i in range(1, 5):
     devices = dict()
     devices["name"] = (
         choice(["r2", "r3", "r4", "r6", "r10"])
@@ -18,5 +18,8 @@ for i in range(0, 5):
     elif devices["vendor"] == "juniper":
         devices["os"] = "junos"
         devices["version"] = choice(["J6.23.1", "8.43.12", "6.45", "6.03"])
-
+    elif devices["vendor"] == "asista":
+        devices["os"] = "eos"
+        devices["version"] = choice(["a", "b", "c"])
+    devices["ip"] = "10.0.0." + str(i)
     print(devices)
