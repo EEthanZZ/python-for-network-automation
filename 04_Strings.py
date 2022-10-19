e3723fe23d25bd19dd126789b76ad21dbbb587d8
Ethan <yczhu@yahoo.com>
2022-10-19 04:28:00 -0700

    remove the space at head and end by using method strip()

--- 04_Strings.py
+++ 04_Strings.py
@@ -1,6 +1,11 @@
 device1_str = "  r3-L-n7, cisco, catalyst 2960, ios  "
 
-#SPLIT
+# SPLIT
 device1 = device1_str.split(",")
 print("device1 using split:")
+print("    ", device1)
+
+# STRIP
+device1 = device1_str.strip().split(",")
+print("device1 using strip() and split")
 print("    ", device1)
